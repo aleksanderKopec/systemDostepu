93aec87371eb8d20389e704de1ff34369eb114bf
Alek - nqaq <a.kopec.b@gmail.com>
2021-05-30 19:05:37 +0200

    Added code base access Added code-change functionality Optimized some things Made some additional small changes

--- systemDostepu.ino
+++ systemDostepu.ino
@@ -26,6 +26,9 @@ LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 //not the end of last admin card address.
 #define ADMIN_ADDRESS 0
 
+//adress to store access_code
+#define CODE_ADDRESS 1020
+
 //State variable declaration. The program will start in this state. Default 0.
 //Change to 1 for adding state or 2 for deleting state.
 short state = 0;
@@ -209,7 +212,6 @@ bool checkIfCardFound(){
 
     irqCurr = digitalRead(PN532_IRQ);
 
-
     if (irqCurr == LOW && irqPrev == HIGH) {
         Serial.println("Got NFC IRQ");  
         return true;
@@ -225,6 +227,21 @@ void sendSignalToStrike(const int pin, const int seconds = 10){
     noTone(BUZZ_PIN);
 }
 
+bool ifChangeState(){
+    if (ButtonRead(analogRead(A0)) == 'r'){
+        state++;
+        if (state == 5) state = 0;
+        return true;
+    }
+
+    else if (ButtonRead(analogRead(A0)) == 'l'){
+        state--;
+        if (state == -1) state = 4;
+        return true;
+    }
+    return false;
+}
+
 void standardState(){
     lcd.clear();
     startListeningToNFC();      // start listening
@@ -236,21 +253,9 @@ void standardState(){
 
         lcd.print("Zbliz karte");    
 
-
-        //For controlling the flow of program, don't change if not necessary
-        //BEGIN CONTROL BLOCK
-        if (ButtonRead(analogRead(A0)) == 'r'){
-            state = 1;
+        if (ifChangeState()){
             return;
         }
-
-        else if (ButtonRead(analogRead(A0)) == 'l'){
-            state = 2;
-            return;
-        }
-        //END CONTROL BLOCK
-
-        //Below is the true functionality of this state
 
         //initialise the variables needed in this state
         uint8_t success = false;
@@ -322,21 +327,10 @@ void addingState(){
         lcd.setCursor(0, 1);
         lcd.print("administratora");
 
-
-        //For controlling the flow of program, don't change if not necessary
-        //BEGIN CONTROL BLOCK
-        if (ButtonRead(analogRead(A0)) == 'r'){
-            state = 2;
+        if (ifChangeState()){
             return;
         }
 
-        else if (ButtonRead(analogRead(A0)) == 'l'){
-            state = 0;
-            return;
-        }
-        //END CONTROL BLOCK
-
-        //Below is the true code of this state
         uint8_t success = false;
         uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
         uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
@@ -386,6 +380,7 @@ void addingState(){
                             lcd.print("Zbliz druga");
                             lcd.setCursor(0, 1);
                             lcd.print("karte");
+                            //delay(50);
 
                             if(checkIfCardFound()){
 
@@ -422,8 +417,6 @@ void addingState(){
                                         break;
                                     }
 
-                                
-
                                 }
 
                                 if(success && checkUID(uid, uidLength) != -1){
@@ -440,18 +433,9 @@ void addingState(){
                                 }
                             }
 
-                            //For controlling the flow of program, don't change if not necessary
-                            //BEGIN CONTROL BLOCK
-                            if (ButtonRead(analogRead(A0)) == 'r'){
-                                state = 2;
+                            if (ifChangeState()){
                                 return;
                             }
-
-                            else if (ButtonRead(analogRead(A0)) == 'l'){
-                                state = 0;
-                                return;
-                            }
-                            //END CONTROL BLOCK
                         }
                     }
 
@@ -489,19 +473,9 @@ void deletingState(){
         lcd.setCursor(0, 1);
         lcd.print("administratora");
 
-
-        //For controlling the flow of program, don't change if not necessary
-        //BEGIN CONTROL BLOCK
-        if (ButtonRead(analogRead(A0)) == 'r'){
-            state = 0;
+        if (ifChangeState()){
             return;
         }
-
-        else if (ButtonRead(analogRead(A0)) == 'l'){
-            state = 1;
-            return;
-        }
-        //END CONTROL BLOCK
 
         uint8_t success = false;
         uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
@@ -591,20 +565,290 @@ void deletingState(){
                                 }
                             }
 
-                            //For controlling the flow of program, don't change if not necessary
-                            //BEGIN CONTROL BLOCK
-                            if (ButtonRead(analogRead(A0)) == 'r'){
-                                state = 0;
+                            if (ifChangeState()){
                                 return;
                             }
 
-                            else if (ButtonRead(analogRead(A0)) == 'l'){
-                                state = 1;
-                                return;
-                            }
-                            //END CONTROL BLOCK
+                            lcd.clear();
 
                         }
+                    }
+
+                    else{
+                        Serial.println("Invalid admin card, try again");
+                        lcd.clear();
+                        lcd.print("To nie karta");
+                        lcd.setCursor(0,1);
+                        lcd.print("administratora");
+                        delay(1000);
+                        lcd.clear();
+                    }
+                    
+                } 
+            }
+
+            startListeningToNFC(); //restart the listening utility
+
+        }
+        
+
+
+    delay(100); //temporary delay, subject to change
+    lcd.clear();
+    }
+}
+
+void powiekszLiczbe(uint8_t arr[], short ktora){
+    arr[ktora]++;
+    if (arr[ktora] == 10){
+        arr[ktora] = 0;
+    }
+}
+
+void pomniejszLiczbe(uint8_t arr[], short ktora){
+    arr[ktora]--;
+    if(arr[ktora] == 255){
+        arr[ktora] = 9;
+    }
+}
+
+void printCodeAndOptions(uint8_t code[], char message[]){
+    lcd.print(message);
+    lcd.setCursor(0, 1);
+
+    lcd.print(code[0]);
+    lcd.print(' ');
+
+    lcd.print(code[1]);
+    lcd.print(' ');
+
+    lcd.print(code[2]);
+    lcd.print(' ');
+
+    lcd.print(code[3]);
+    lcd.print("  ");
+
+    lcd.print("OK");
+    lcd.print(' ');
+    lcd.print("END");
+}
+
+bool codeInput(uint8_t code[], char message[]){
+    lcd.clear();
+    short curr_selection = 0;
+    printCodeAndOptions(code, message);
+    while (true){
+
+        if (curr_selection == 4){
+            lcd.setCursor(9, 1);
+        }
+        else if (curr_selection == 5){
+            lcd.setCursor(12, 1);
+        }
+        else{
+            lcd.setCursor(curr_selection * 2, 1);
+        }
+        
+        lcd.cursor();
+
+        if (ButtonRead(analogRead(A0)) == 'u'){
+            powiekszLiczbe(code, curr_selection);
+            lcd.clear();
+            printCodeAndOptions(code, message);
+            delay(300);
+        }
+
+        if (ButtonRead(analogRead(A0)) == 'd'){
+            pomniejszLiczbe(code, curr_selection);
+            lcd.clear();
+            printCodeAndOptions(code, message);
+            delay(300);
+        }
+
+        if (ButtonRead(analogRead(A0)) == 'l'){
+            curr_selection--;
+            if (curr_selection == -1){
+                curr_selection = 5;
+            }
+            delay(600);
+        }
+
+        if (ButtonRead(analogRead(A0)) == 'r'){
+            curr_selection++;
+            if (curr_selection == 6){
+                curr_selection = 0;
+            }
+            delay(600);
+        }
+
+        if (ButtonRead(analogRead(A0)) == 's'){
+            if (curr_selection == 4){
+                return true;
+            }
+            else if (curr_selection == 5){
+                return false;
+            }
+        }
+
+
+
+    }
+}
+
+bool checkIfArraysEqual(uint8_t arr1[], uint8_t arr2[], const short length){
+    for (int i = 0; i < length; i++){
+        if (arr1[i] != arr2[i]){
+            return false;
+        }
+    }
+    return true;
+}
+
+void codeState(){
+    lcd.clear();
+    uint8_t obecny_kod[] = { 0, 0, 0, 0 };
+    uint8_t kod_dostepu[] = { 0, 0, 0, 0};
+
+    readUID(kod_dostepu, CODE_ADDRESS, 4);
+    while(true){
+
+
+        Serial.println("Code state");
+
+        char message[] = "PODAJ KOD";
+        bool result = codeInput(obecny_kod, message);
+        lcd.noCursor();
+        lcd.clear();
+        if (!result){
+            lcd.print("Przerwano wpi-");
+            lcd.setCursor(0, 1);
+            lcd.print("sywanie kodu");
+            state = 4;
+            delay(1000);
+            lcd.clear();
+            return;
+        }
+        
+        
+        lcd.clear();
+
+        if (!checkIfArraysEqual(obecny_kod, kod_dostepu, 4)){
+            lcd.print("Niepoprawny kod");
+            delay(1000);
+            lcd.clear();
+            lcd.print("Sprobuj jeszcze");
+            lcd.setCursor(0, 1);
+            lcd.print("raz.");
+            state = 3;
+            delay(1000);
+            lcd.clear();
+            return;
+        }
+        else{
+            lcd.print("Poprawny kod");
+            lcd.setCursor(0, 1);
+            lcd.print("Drzwi otwarte");
+            state = 4;
+            sendSignalToStrike(CONTROL_PIN);
+            lcd.clear();
+        }
+
+        
+        return;
+
+    }
+}
+
+void changeCodeState(){
+    lcd.clear();
+    while (true) {
+
+        Serial.println("Deleting State");
+
+        lcd.print("Zbliz karte");
+        lcd.setCursor(0, 1);
+        lcd.print("administratora");
+
+        if (ifChangeState()){
+            return;
+        }
+
+        uint8_t success = false;
+        uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
+        uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+
+        Serial.println("Waiting for Admin card");
+
+        if (checkIfCardFound()){                                        //if the card is found
+
+            success = nfc.readDetectedPassiveTargetID(uid, &uidLength); //read the card
+
+            if (success){
+                
+                //DEBUG
+                Serial.println("Found card");
+                Serial.print("  UID Value: ");
+                for(short i = 0; i < uidLength; i++){
+                    Serial.print(uid[i]);
+                    Serial.print(" ");
+                }
+                Serial.println("\n");
+                //END DEBUG
+
+                if(uidLength != 4){                                     //if card is invalid do nothing
+                    Serial.println("Invalid card");                     //expected length of card uid is 4
+                }
+
+                else{                                                   //if card is valid
+
+                    if(checkUID(uid, uidLength) <= ADMIN_ADDRESS && checkUID(uid, uidLength) != -1){      //if card is admin card
+
+                        Serial.println("Valid Admin card found");
+
+                        lcd.clear();
+                        lcd.print("Podano karte");
+                        lcd.setCursor(0, 1);
+                        lcd.print("administratora");
+
+                        delay(1000);
+                        lcd.clear();
+
+                        uint8_t nowy_kod[] = { 0, 0, 0, 0 };
+                        char message[] = "PODAJ NOWY KOD:";
+                        bool result = codeInput(nowy_kod, message);
+
+                        lcd.noCursor();
+                        lcd.clear();
+                        if (!result){
+                            lcd.print("Przerwano wpi-");
+                            lcd.setCursor(0, 1);
+                            lcd.print("sywanie kodu");
+                            delay(1000);
+                            lcd.clear();
+                            state = 0;
+                            startListeningToNFC();
+                            return;
+                        }
+
+
+                        lcd.clear();
+                        lcd.print("Podano nowy kod");
+                        lcd.setCursor(0, 1);
+                        lcd.print("Zapisywanie...");
+                        writeUID(nowy_kod, CODE_ADDRESS, 4);
+                        delay(1000);
+
+                        lcd.clear();
+                        lcd.print("Zapis wykonany");
+                        lcd.setCursor(0, 1);
+                        lcd.print("pomyslnie");
+                        delay(1000);
+
+                        lcd.clear();
+                        state = 0;
+                        startListeningToNFC();
+                        return;
+                        
                     }
 
                     else{
@@ -637,24 +881,6 @@ void setup(){
     lcd.begin(16, 2);
     lcd.print("Starting up");
     Serial.println("Starting up");
-
-    /*
-    int address = 0;
-    uint8_t arr[] = {0, 0, 0, 0};
-    short length = 4;
-    Serial.print("\n");
-    while(address < 1024){
-        address = readUID(arr, address, length);
-        for(short i = 0; i < length; i++){
-            Serial.print(arr[i]);
-            Serial.print(" ");
-        }
-        Serial.print("\n");
-    }
-
-    */
-
-    
 
     //Starting the i2c communication and printing some debug info
     //BEGIN i2c BLOCK
@@ -692,6 +918,7 @@ void loop(){
     Serial.print("\n");
     //END DEBUG
 
+    startListeningToNFC();
 
     //this code controls flow of the program, don't change if not necessary
     //BEGIN CONTROL BLOCK
@@ -723,6 +950,22 @@ void loop(){
             lcd.clear();
             deletingState();
         break;
+
+        case 3:
+            delay(400);
+            lcd.clear();
+            lcd.print("Wpisywanie kodu");
+            delay(1000);
+            lcd.clear();
+            codeState();
+
+        case 4:
+            delay(400);
+            lcd.clear();
+            lcd.print("Zmiana kodu");
+            delay(1000);
+            lcd.clear();
+            changeCodeState();   
     }
     //END CONTROL BLOCK
 }
